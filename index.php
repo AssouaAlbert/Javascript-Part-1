59e1e50ceb2104e9fca2c077556a83fe99ea3a15
AssouaAlbert <assouaalbert@gmail.com>
2020-01-11 13:17:52 +0300

    added php code to display files in diferent directories

--- index.php
+++ index.php
@@ -9,49 +9,34 @@
 </head>
 
 <body>
-    <ul>
-        <li><a href="./15+ tricks to be array Expert.html">15+ tricks to be array Expert</a></li>
-        <li><href="./25 Change Image onmouseover and onmouseout events in JavaScript.html">25 Change Image onmouseover and onmouseout events in JavaScript</a></li>
-        <li><a href="./26 Form Validations in JavaScript.html">26 Form Validations in JavaScript</a></li>
-        <li><a href="./27 Form Validations in JavaScript part 2.html">27 Form Validations in JavaScript part 2</a></li>
-        <li><a href="./28 Regular Expressions in JavaScript Part 1.html">28 Regular Expressions in JavaScript Part 1</a></li>
-        <li><a href="./30 Mobile Number Validation in JavaScript using Regular Expression.html">30 Mobile Number Validation in JavaScript using Regular Expression</a></li>
-        <li><a href="./31 Email Validation in JavaScript.html">31 Email Validation in JavaScript</a></li>
-        <li><a href="./33 Timing Functions in JavaScript.html">33 Timing Functions in JavaScript</a></li>
-        <li><a href="./34_fade_in_fade_out_animations_in_javascript.html">34_fade_in_fade_out_animations_in_javascript</a></li>
-        <li><a href="./35_zoom_in_zoom_out_effect_in_javascript.html">35_zoom_in_zoom_out_effect_in_javascript</a></li>
-        <li><a href="./36_introduction_to_jquery_library_setting_jquery_in_project.html">36_introduction_to_jquery_library_setting_jquery_in_project</a></li>
-        <li><a href="./37_jquery_selectors_part_1.html">37_jquery_selectors_part_1</a></li>
-        <li><a href="./38_jquery_selectors_part_2.html">38_jquery_selectors_part_2</a></li>
-        <li><a href="./39_events_in_jquery_part_1.html">39_events_in_jquery_part_1</a></li>
-        <li><a href="./40_events_in_jquery_part_2.html">40_events_in_jquery_part_2</a></li>
-        <li><a href="./41_effects_in_jquery_part_1.html">41_effects_in_jquery_part_1</a></li>
-        <li><a href="./42_jquery_animate_effect.html">42_jquery_animate_effect</a></li>
-        <li><a href="./43_callback_and_chaining_in_jquery_effects.html">43_callback_and_chaining_in_jquery_effects</a></li>
-        <li><a href="./44_jquery_dom_manipulations_part_1.html">44_jquery_dom_manipulations_part_1</a></li>
-        <li><a href="./46_jquery_dom_manipulations_part_3.html">46_jquery_dom_manipulations_part_3</a></li>
-        <li><a href="./48_jquery_ui_including_jquery_ui_in_project.html">48_jquery_ui_including_jquery_ui_in_project</a></li>
-        <li><a href="./57 draggable interaction in jquery ui.html">57 draggable interaction in jquery ui</a></li>
-        <li><a href="./58 sortable_interaction_in_jquery_ui.html">58 sortable_interaction_in_jquery_ui</a></li>
-        <li><a href="./8ThingsToNotes.html">8ThingsToNotes</a></li>
-        <li><a href="./Accordion Widget Control in JQuery UI.html">Accordion Widget Control in JQuery UI</a></li>
-        <li><a href="./ArraysandObjects.html">ArraysandObjects</a></li>
-        <li><a href="./Autocomplete Widget in JQuery UI.html">Autocomplete Widget in JQuery UI</a></li>
-        <li><a href="./Date Picker Widget Control in JQuery UI.html">Date Picker Widget Control in JQuery UI</a></li>
-        <li><a href="./DOMimputs.html">DOMimputs</a></li>
-        <li><a href="./DOMUsingSelectBox.html">DOMUsingSelectBox</a></li>
-        <li><a href="./DOMUsingSelectBox.html">DOMUsingSelectBox</a></li>
-        <li><a href="./GetElementByTagName.html">GetElementByTagName</a></li>
-        <li><a href="./Inputs from Users.html">Inputs from Users</a></li>
-        <li><a href="./Javscriptpart1html.html">Javscriptpart1html</a></li>
-        <li><a href="./Message box widget in jquery.html">Message box widget in jquery</a></li>
-        <li><a href="./Objects.html">Objects</a></li>
-        <li><a href="./Objects2.html">Objects2</a></li>
-        <li><a href="./Starting With Dom.html">Starting With Dom</a></li>
-        <li><a href="./Success Form Validations in JavaScript.html">Success Form Validations in JavaScript</a></li>
-        <li><a href="./tooltip_widget_control_in_jquery_ui_.html">tooltip_widget_control_in_jquery_ui_</a></li>
-        <li><a href=""></a></a></li>
-    </ul>
+    <h1>Udemy Javascript</h1>
+    <?php
+    $directory = "./JS Udemy";
+
+if (is_dir($directory)){
+  if ($opendirectory = opendir($directory)){
+    while (($file = readdir($opendirectory)) !== false){
+      echo "<a href='http://eloumbatassouaalbert.com/$directory" . "$file'>$file</a>"."<br>";
+    }
+    closedir($opendirectory);
+  }
+}
+?>
+    <h1>
+        Youtube videos
+    </h1>
+    <?php
+$directory = "./";
+
+if (is_dir($directory)){
+  if ($opendirectory = opendir($directory)){
+    while (($file = readdir($opendirectory)) !== false){
+      echo "<a href='http://eloumbatassouaalbert.com/$directory" . "$file'>$file</a>"."<br>";
+    }
+    closedir($opendirectory);
+  }
+}
+?>
 
 
 </body>
